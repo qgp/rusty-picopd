e80b342de898db5c02c247ba54c75e948a4acbc6
Jochen Klein <jochen.klein@cern.ch>
2024-05-19 23:00:22 +0200

    Extend register definitions and negotiation

--- src/bin/pd.rs
+++ src/bin/pd.rs
@@ -29,14 +29,14 @@ bind_interrupts!(struct Irqs {
 
 const ADDR: u8 = 0x51;
 
-bitfield!{
+bitfield! {
     pub struct Status(u8);
     impl Debug;
     derating, _: 7;
     otp, _: 6;
     ocp, _: 5;
     ovp, _: 4;
-    newpdo, _: 2;
+    newpdos, _: 2;
     success, _: 1;
     ready, _: 0;
 }
@@ -51,18 +51,67 @@ bitfield! {
 bitfield! {
     pub struct APDO(u32);
     impl Debug;
-    vmin, _: 24, 17;
-    vmax, _: 15, 8;
+    vmax, _: 24, 17;
+    vmin, _: 15, 8;
     imax, _: 6, 0;
+}
+
+enum PDO {
+    FixedPDO(FixedPDO),
+    APDO(APDO),
+}
+
+bitfield! {
+    pub struct FixedRDO(u32);
+    impl Debug;
+    _, pos: 30, 28;
+    _, i: 19, 10; // LSB 10 mA
+    _, imax: 9, 0; // LSB 10 mA
+}
+
+bitfield! {
+    pub struct ARDO(u32);
+    impl Debug;
+    _, pos: 30, 28;
+    _, volt: 19, 9; // LSB 20 mV
+    _, i: 6, 0; // LSB 50 mA
+}
+
+enum RDO {
+    FixedRDO(FixedRDO),
+    ARDO(ARDO),
+}
+
+impl RDO {
+    fn reg(&self) -> &u32 {
+        match self {
+            RDO::FixedRDO(v) => &v.0,
+            RDO::ARDO(v) => &v.0,
+        }
+    }
 }
 
 struct AP33772<I2C> {
     i2c: I2C,
+    pdos: [Option<PDO>; 7],
+    status: Status,
 }
 
 impl<I2C: I2c_block> AP33772<I2C> {
     pub fn new(usb_dev: I2C) -> Self {
-        Self { i2c: usb_dev }
+        Self {
+            i2c: usb_dev,
+            pdos: [None, None, None, None, None, None, None],
+            status: Status(0),
+        }
+    }
+
+    pub fn update(&mut self) -> Result<(), I2C::Error> {
+        self.status.0 = self.read_status()?;
+        if self.status.ready() && self.status.newpdos() {
+            self.read_pdos();
+        }
+        Ok(())
     }
 
     pub fn read_buf<const N: usize>(&mut self, wbuf: &[u8]) -> Result<[u8; N], I2C::Error> {
@@ -77,6 +126,15 @@ impl<I2C: I2c_block> AP33772<I2C> {
         for i in 0..7 {
             let pdo: &[u8; 4] = &buf[4 * i..4 * (i + 1)].try_into().unwrap();
             pdos[i] = u32::from_le_bytes(*pdo);
+            self.pdos[i] = if pdos[i] == 0x0 {
+                None
+            } else if pdos[i] & 0xf000_0000 == 0xc000_0000 {
+                Some(PDO::APDO(APDO(pdos[i])))
+            } else if pdos[i] & 0xc000_0000 == 0x0 {
+                Some(PDO::FixedPDO(FixedPDO(pdos[i])))
+            } else {
+                None
+            };
         }
         Ok(pdos)
     }
@@ -110,15 +168,11 @@ impl<I2C: I2c_block> AP33772<I2C> {
         Ok(buf[0])
     }
 
-    pub fn write_rdo(&mut self) -> Result<(), I2C::Error> {
-        let mut buf = bv::bitarr![u8, bv::prelude::Lsb0; 0; 32];
-        // fixed PDO
-        buf[31..=31].store(0); // reserved
-        buf[28..=30].store(0); // object position
-        buf[20..=27].store(0); // reserved
-        buf[10..=19].store(100); // operating current
-        buf[0..=9].store(100); // max operating current
-        self.i2c.write(ADDR, buf.as_raw_slice())
+    pub fn write_rdo(&mut self, rdo: &RDO) -> Result<(), I2C::Error> {
+        let mut buf = [0u8; 5];
+        buf[0] = 0x30;
+        buf[1..5].copy_from_slice(&rdo.reg().to_le_bytes());
+        self.i2c.write(ADDR, &buf)
     }
 
     pub fn reset(&mut self) -> Result<(), I2C::Error> {
@@ -131,6 +185,7 @@ impl<I2C: I2c_block> AP33772<I2C> {
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
+    let mut pwr_en = gpio::Output::new(p.PIN_23, gpio::Level::Low);
     let mut led = gpio::Output::new(p.PIN_25, gpio::Level::Low);
     spawner.spawn(blink_led(led)).unwrap();
 
@@ -138,7 +193,7 @@ async fn main(spawner: Spawner) {
     let mut config = Config::new(0x1556, 0xcafe);
     config.manufacturer = Some("qgp.io");
     config.product = Some("picoPD");
-    config.serial_number = Some("12345678");
+    config.serial_number = Some("12345678"); // user s/n of flash: https://docs.rs/embassy-rp/latest/embassy_rp/flash/struct.Flash.html#method.blocking_unique_id
     config.max_power = 100;
     config.max_packet_size_0 = 64;
     let mut config_descriptor = [0; 256];
@@ -167,6 +222,27 @@ async fn main(spawner: Spawner) {
     let mut pdc = AP33772::new(bus_i2c::RefCellDevice::new(&i2c_ref_cell));
     Timer::after_secs(1).await;
     let status_boot = pdc.read_status().unwrap();
+    pdc.read_pdos();
+
+    let mut frdo = FixedRDO(0);
+    frdo.pos(2);
+    frdo.i(100);
+    frdo.imax(100);
+
+    let mut ardo = ARDO(0);
+    ardo.pos(6);
+    ardo.volt(3360 / 20);
+    ardo.i(1000 / 50);
+
+    // let rdo = RDO::FixedRDO(frdo);
+    let rdo = RDO::ARDO(ardo);
+    pdc.write_rdo(&rdo);
+    Timer::after_secs(1).await;
+    pdc.update();
+    if pdc.status.ready() && pdc.status.success() {
+        info!("Enabling output");
+        pwr_en.set_high();
+    }
 
     // let echo_fut = async {
     //     loop {
@@ -206,28 +282,29 @@ async fn main(spawner: Spawner) {
             // sender.write_packet(b"; ").await;
             // sender.write_packet(sbuf.format(npdos).as_bytes()).await;
             // sender.write_packet(b"\n").await;
-            for i in 0..npdos as usize {
-                if pdos[i] & 0xc000_0000 == 0 {
-                    let fpdo = FixedPDO(pdos[i]);
-                    info!(
-                        "pdo[{}]: 0x{:08x} -> fixed: {} mV, {} mA",
-                        i,
-                        pdos[i],
-                        fpdo.vmax() * 50,
-                        fpdo.imax() * 10,
-                    );
-                } else if pdos[i] & 0xf000_0000 == 0xc000_0000 {
-                    info!(
-                        "pdo[{}]: 0x{:08x} -> PPS: {} - {} mV, {} mA",
-                        i,
-                        pdos[i],
-                        pdos[i].view_bits::<Lsb0>()[17..=24].load::<u32>() * 100,
-                        pdos[i].view_bits::<Lsb0>()[8..=15].load::<u32>() * 100,
-                        pdos[i].view_bits::<Lsb0>()[0..=6].load::<u32>() * 50,
-                    );
+            for (i, pdo) in pdc.pdos.iter().enumerate() {
+                match pdo {
+                    Some(PDO::FixedPDO(fpdo)) => {
+                        info!(
+                            "pdo[{}]: 0x{:08x} -> fixed: {} mV, {} mA",
+                            i + 1,
+                            fpdo.0,
+                            fpdo.vmax() * 50,
+                            fpdo.imax() * 10,
+                        );
+                    }
+                    Some(PDO::APDO(apdo)) => {
+                        info!(
+                            "pdo[{}]: 0x{:08x} -> PPS: {} - {} mV, {} mA",
+                            i + 1,
+                            apdo.0,
+                            apdo.vmin() * 100,
+                            apdo.vmax() * 100,
+                            apdo.imax() * 50,
+                        );
+                    }
+                    _ => {}
                 }
-                //     sender.write_packet(sbuf.format(pdos[i]).as_bytes()).await;
-                //     sender.write_packet(b"\n").await;
             }
             Timer::after_secs(5).await;
         }
